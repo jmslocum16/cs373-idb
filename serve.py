6177b039e020f9a02aabeac8d784adb2b9056225
KevinHino <kevin.hino@utexas.edu>
2015-03-24 02:18:13 -0500

    serve.py works locally, need to test it on the server.

--- serve.py
+++ serve.py
@@ -1,32 +1,45 @@
 import os.path
 
 from flask import Flask
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_folder='static_html')
+app.debug = False
+
+STATIC_FOLDER = './static_html/' 
 
 
 @app.route('/')
+def root():
+    return app.send_static_file('splash.html')
+
+@app.route('/splash.html')
 def splash():
-    #return "Hello world"
-    return app.send_from_directory('/static_html', filename="splash.html")
+    return app.send_static_file('splash.html')
 
-@app.route('/player/<player_id>')
+@app.route('/about.html')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/sorttable.js')
+def sorttable():
+    return app.send_static_file('sorttable.js')
+
+@app.route('/players/<player_id>')
 def get_player_page(player_id):
-    if os.path.isfile('/static_html/players/' + player_id):
-        return send_from_directory('/players/', filename=player_id)
-    #send_from_directory('/', filename='404.html')
+    static_file = 'players/' + player_id
+    if os.path.isfile(STATIC_FOLDER + static_file):
+        return app.send_static_file(static_file)
 
-@app.route('/season/<season_id>')
+@app.route('/seasons/<season_id>')
 def get_season_page(season_id):
-    if os.path.isfile('/static_html/seasons/' + season_id):
-        return send_from_directory('/seasons/', filename=season_id)
-    #send_from_directory('/', filename='404.html')
+    static_file = 'seasons/' + season_id
+    if os.path.isfile(STATIC_FOLDER + static_file):
+        return app.send_static_file(static_file)
 
-@app.route('/team/<team_id>')
+@app.route('/teams/<team_id>')
 def get_team_page(team_id):
-    if os.path.isfile('/static_html/teams/' + team_id):
-        return send_from_directory('/teams/', filename=team_id)
-    #send_from_directory('/', filename='404.html')
-
+    static_file = 'teams/' + team_id
+    if os.path.isfile(STATIC_FOLDER + static_file):
+        return app.send_static_file(static_file)
 
 if __name__ == "__main__":
     app.run()
