1ec594c1a0711eec4daec84357707d68984b0f84
jmslocum16 <jmslocum16@yahoo.com>
2015-03-25 12:56:28 -0500

    added field documentation to models

--- Models.py
+++ Models.py
@@ -3,27 +3,65 @@
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
 
+# TODO include relationship fields in attributes?
+
 """
-Stats model
-TODO
+Stats model. Contains all of the statistics for a given stat line.
+Attributes:
+   stat_id The unique id of this stat line.
+   player_id The id of the player this stat line belongs to, or None if it is a team stat line.
+   team_id The id of the team this stat line belongs to, or the team of the player this stat line belongs to.
+   season The season id that this stat line is from.
+   gp Total games played during the season.
+   wins Total games won during the season.
+   losses Total games lost during the season.
+   pct Win percentage during the season.
+   mins The average minutes played per game during the season.
+   fgm The average field goals made per game during the season.
+   fga The average field goals attempted per game during the season.
+   fg3m The average three-point field goals made per game during the season.
+   fg3a The average three-point field goals attempted per game during the season.
+   fg3pct The total three-point field goal percentage during the season.
+   ftm The average free throws made per game during the season.
+   fta The average free throws attempted per game during the season.
+   ftpct The total free throw percentage during the season.
+   oreb The average offensive rebounds per game during the season.
+   dreb The average defensive rebounds per game during the season.
+   reb The average rebounds per game during the season.
+   ass The average assists per game during the season.
+   tov The average turnovers per game during the season.
+   stl The average steals per game during the season.
+   blk The average blocks per game during the season.
+   blka The average blocks attempted per game during the season.
+   pf The average personal fouls per game during the season.
+   pfd The average defensive personal fouls per game during the season.
+   pts The average points scored per game during the season.
+   plusminus The average plus/minus per game during the season.
 """
 StatLine = None
 
 """
-Player Model
-TODO
+Player Model. Contains all of the information for a specific player.
+Attributes:
+    player_id The id of the player.
+    name The name of the player.
+    nba_team_team_id The team_id of the most recent team this player belongs to.
 """
 Player = None
 
 """
-Team Model
-TODO
+Team Model. Contains all of the information for a specific team.
+Attributes:
+    team_id The id of the team.
+    name The name of the team.
+    abrv The three letter abbreviation the NBA uses for each team.
 """
 Team = None
 
 """
-Season Model
-TODO
+Season Model. Contains all of the information for a given season.
+Attributes:
+    season_id The id of the season.
 """
 Season = None
 
@@ -36,6 +74,7 @@ def loadModels() :
     global Player
     global Team
     global Season
+
     Base = automap_base()
 
     Engine = create_engine("postgresql://postgres:nbaproject@23.253.119.99:5432/test")
